e6a8ae725041fd3e9488c5b4f09329d222adddf0
John Knowles <jtknowles007@gmail.com>
2020-05-02 00:40:12 -0400

    Fixed broken download

--- ytaudiogui.py
+++ ytaudiogui.py
@@ -5,41 +5,46 @@ import sys
 import PySimpleGUI as sg
 import youtube_dl
 
-
-if len(sys.argv) ==1:
-    event, values = sg.Window('YTaudio',
+window = sg.Window('YTaudio',
             [[sg.Text('Playlist URL')],
             [sg.In()],
             [sg.Text('Directory Name')],
             [sg.In()],
-            [sg.Ok(), sg.Cancel()]], location=(0,0)).read(close=True)
-    playlisturl = values[0] 
-    playlistdir = values[1]
-else:
-# Pass the playlist URL and directory as arguments to the script call
-    playlisturl = sys.argv[1]
-    playlistdir = sys.argv[2]
+            [sg.Button('Go'), sg.Button('Close')]], location=(0,0))
+while True:
+    event,values = window.read()
+    if event in (None,'Close'):
+        break
+    elif event in ('Go'):
 
-theplaylist = '/home/john/Music/{}'.format(playlistdir)
-# Create directory if not present
-if not os.path.isdir(theplaylist):
-    os.mkdir(theplaylist)
+        playlisturl = values[0] 
+        playlistdir = values[1]
 
-# Format filename for saved audio files
-output = '{}/%(playlist_index)s-%(title)s.%(ext)s'.format(theplaylist)
+        theplaylist = '/home/john/Music/{}'.format(playlistdir)
+        # Create directory if not present
+        if not os.path.isdir(theplaylist):
+            os.mkdir(theplaylist)
 
-# Output options
-options = {
-        'format':'bestaudio/best',
-        'postprocessors':[{
-            'key':'FFmpegExtractAudio',
-            'preferredcodec':'mp3',
-            'preferredquality':'192',
-            }],
-        'quiet': False,
-        'restrictfilenames': True,
-        'outtmpl':output}
+        # Format filename for saved audio files
+        output = '{}/%(playlist_index)s-%(title)s.%(ext)s'.format(theplaylist)
 
-# Download and convert the Youtube audio
-with youtube_dl.YoutubeDL(options) as ydl:
-    ydl.download([playlisturl])
+        # Output options
+        options = {
+            'format':'bestaudio/best',
+            'postprocessors':[{
+                'key':'FFmpegExtractAudio',
+                'preferredcodec':'mp3',
+                'preferredquality':'192',
+                }],
+            'quiet': False,
+            'restrictfilenames': True,
+            'continue_dl':True,
+            'ignoreerrors':True,
+            'outtmpl':output}
+
+        # Download and convert the Youtube audio
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.cache.remove()
+            ydl.download([playlisturl])
+window.close()
+
