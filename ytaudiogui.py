b303a8b1b1e15f426474fe387326e59a9b1c0ffa
John Knowles <jtknowles007@gmail.com>
2020-05-02 00:46:20 -0400

    Create and call youtube_dl via function

--- ytaudiogui.py
+++ ytaudiogui.py
@@ -5,21 +5,7 @@ import sys
 import PySimpleGUI as sg
 import youtube_dl
 
-window = sg.Window('YTaudio',
-            [[sg.Text('Playlist URL')],
-            [sg.In()],
-            [sg.Text('Directory Name')],
-            [sg.In()],
-            [sg.Button('Go'), sg.Button('Close')]], location=(0,0))
-while True:
-    event,values = window.read()
-    if event in (None,'Close'):
-        break
-    elif event in ('Go'):
-
-        playlisturl = values[0] 
-        playlistdir = values[1]
-
+def dlfunct(playlisturl,playlistdir):
         theplaylist = '/home/john/Music/{}'.format(playlistdir)
         # Create directory if not present
         if not os.path.isdir(theplaylist):
@@ -46,5 +32,20 @@ while True:
         with youtube_dl.YoutubeDL(options) as ydl:
             ydl.cache.remove()
             ydl.download([playlisturl])
+
+window = sg.Window('YTaudio',
+            [[sg.Text('Playlist URL')],
+            [sg.In()],
+            [sg.Text('Directory Name')],
+            [sg.In()],
+            [sg.Button('Go'), sg.Button('Close')]], location=(0,0))
+while True:
+    event,values = window.read()
+    if event in (None,'Close'):
+        break
+    elif event in ('Go'):
+        plurl = values[0] 
+        pldir = values[1]
+        dlfunct(plurl,pldir)
 window.close()
 
