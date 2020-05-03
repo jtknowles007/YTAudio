b26f9da6ceed87b30cc2f805d2ad155b56225c7e
John Knowles <jtknowles007@gmail.com>
2020-05-03 12:13:12 -0400

    Thread/Status update output

--- ytaudiogui.py
+++ ytaudiogui.py
@@ -2,10 +2,12 @@
 
 import os
 import sys
+import queue
+import threading
 import PySimpleGUI as sg
 import youtube_dl
 
-def dlfunct(playlisturl,playlistdir):
+def dlfunct(playlisturl,playlistdir,q):
         theplaylist = '/home/john/Music/{}'.format(playlistdir)
         # Create directory if not present
         if not os.path.isdir(theplaylist):
@@ -31,25 +33,38 @@ def dlfunct(playlisturl,playlistdir):
         # Download and convert the Youtube audio
         with youtube_dl.YoutubeDL(options) as ydl:
             ydl.cache.remove()
+            q.put('Remove cache')
+            q.put('Begin downloads')
             ydl.download([playlisturl])
+            q.put('Complete')
 
 def the_gui():
+    sg.theme('Light Brown 3')
+    gui_queue = queue.Queue()
     window = sg.Window('YTaudio',
             [[sg.Text('Playlist URL')],
             [sg.In()],
             [sg.Text('Directory Name')],
             [sg.In()],
-            [sg.T('Window will close when downloads complete', size=(45,1))],
-            [sg.Button('Go', bind_return_key=True), sg.Button('Close')]], location=(0,0)).Finalize()
+            [sg.Output(size=(70, 12),key='outkey')],
+            [sg.Button('Run', bind_return_key=True), sg.Button('Close')]], location=(0,0))
     while True:
-        event,values = window.read()
+        event,values = window.read(timeout=100)
         if event in (None,'Close'):
             window.close()
             break
-        plurl = values[0] 
-        pldir = values[1]
-        dlfunct(plurl,pldir)
-        window.close()
+        elif event == 'Run':
+            plurl = values[0] 
+            pldir = values[1]
+            threading.Thread(target=dlfunct, args=(plurl,pldir,gui_queue,),daemon=True).start()
+        try:
+            message = gui_queue.get_nowait()
+        except queue.Empty:
+            message = None
+        if message:
+            print(message)
+    window.close()
+    
 
 if __name__ == '__main__':
     the_gui()
